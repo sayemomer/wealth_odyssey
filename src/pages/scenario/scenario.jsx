fc5e4c0c842908d01f730997447648e5dc9cad49
Jerome Kithinji <kithinjijerome@gmail.com>
2025-02-01 21:12:13 -0500

    Scenario baseline done

--- src/pages/scenario/scenario.jsx
+++ src/pages/scenario/scenario.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
+import "./scenario.scss";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { Popover } from "@base-ui-components/react/popover";
+import Checkbox from "@mui/material/Checkbox";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import IconButton from "@mui/material/IconButton";
 import { scenatioData } from "../../assets/scenatioData";
 
 const Scenario = () => {
   const [showHint, setShowHint] = useState(false);
   const scenario = scenatioData[0];
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
 
   return (
     <div className="scenario">
@@ -14,26 +24,33 @@ const Scenario = () => {
         <Typography variant="h3" component="h3">
           Scenario Title
         </Typography>
-        <p className="scenario__text-description">
-          {scenario.summary.text}
-        </p>
+        <p className="scenario__text-description">{scenario.summary.text}</p>
       </div>
       <div className="scenario__factor">
+      <div className="scenario__factor-grid">
+
         {scenario.table.rows.map((row, index) => (
-          <div key={index} className="scenario__factor-item">
-            <p className="scenario__factor-text">{row[0]}</p>
-            <div className="scenario__factor-hint">
-              <Button variant="contained" onClick={() => setShowHint(index === showHint ? null : index)}>
-                Hint
-              </Button>
+          <FormGroup>
+            <div key={index} className="scenario__factor-item">
+              <div className="scenario__factor-hint">
+                <FormControlLabel control={<Checkbox />} label={row[0]} />
+                <IconButton
+                  onClick={() => setShowHint(index === showHint ? null : index)}
+                  size="small"
+                >
+                  <HelpOutlineIcon fontSize="small" />
+                </IconButton>
+              </div>
               {showHint === index && (
                 <div className="scenario__factor-hint__text">
-                  {row[1]} | {row[2]}
+                  <p className="scenario__factor-hint__text-pos">{row[1]}</p>
+                  <p className="scenario__factor-hint__text-neg">{row[2]}</p>
                 </div>
               )}
             </div>
-          </div>
+          </FormGroup>
         ))}
+        </div>
       </div>
       <Button variant="contained" color="success">
         Bullish
