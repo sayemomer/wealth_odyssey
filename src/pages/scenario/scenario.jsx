4307df5d1bc07cec7dc0896fc8992f03d582fb99
Jerome Kithinji <kithinjijerome@gmail.com>
2025-02-02 03:16:10 -0500

    Bone structure added

--- src/pages/scenario/scenario.jsx
+++ src/pages/scenario/scenario.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./scenario.scss";
+import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Checkbox from "@mui/material/Checkbox";
@@ -9,21 +10,30 @@ import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import IconButton from "@mui/material/IconButton";
 import { financialSummaryData } from "../../assets/financialSummaryData";
 import { LineChart } from "@mui/x-charts/LineChart";
+import { scenatioData } from "../../assets/scenatioData";
 
 const Scenario = () => {
   const [currentScenarioIndex, setCurrentScenarioIndex] = useState(0);
   const [showHint, setShowHint] = useState(false);
   const scenario = financialSummaryData[currentScenarioIndex];
+  const [showResponse, setShowResponse] = useState(false);
+  const [buttonsDisabled, setButtonsDisabled] = useState(false);
+  const navigate = useNavigate();
 
-  const handleNext = () => {
-    if (currentScenarioIndex < financialSummaryData.length - 1) {
-      setCurrentScenarioIndex(currentScenarioIndex + 1);
-    }
+  const handleBullishClick = () => {
+    setShowResponse(true);
+    setButtonsDisabled(true);
   };
 
-  const handlePrevious = () => {
-    if (currentScenarioIndex > 0) {
-      setCurrentScenarioIndex(currentScenarioIndex - 1);
+  const handleHomeClick = () => {
+    navigate("/home");
+  };
+
+  const handleReinvest = () => {
+    if (currentScenarioIndex < scenatioData.length - 1) {
+      setCurrentScenarioIndex(currentScenarioIndex + 1);
+      setShowResponse(false);
+      setButtonsDisabled(false);
     }
   };
 
@@ -33,87 +43,109 @@ const Scenario = () => {
   const seriesData = stockData.map((entry) => entry.close); // Closing prices for y-axis
 
   return (
-    <div className="scenario">
-      <div className="scenario__text">
-        <Typography variant="h3" component="h3">
-          {scenario.title}
-        </Typography>
-        <p className="scenario__text-description">{scenario.summary.text}</p>
-      </div>
+    <>
+      <section className="question">
+        <div className="scenario">
+          <div className="scenario__text">
+            <Typography variant="h3" component="h3">
+              {scenario.title}
+            </Typography>
+            <p className="scenario__text-description">
+              {scenario.summary.text}
+            </p>
+          </div>
 
-      <div className="scenario__factor">
-        <div className="scenario__factor-grid">
-          {scenario.table.rows.map((row, index) => (
-            <FormGroup key={index}>
-              <div className="scenario__factor-item">
-                <div className="scenario__factor-hint">
-                  <FormControlLabel control={<Checkbox />} label={row[0]} />
-                  <IconButton
-                    onClick={() =>
-                      setShowHint(index === showHint ? null : index)
-                    }
-                    size="small"
-                  >
-                    <HelpOutlineIcon fontSize="small" />
-                  </IconButton>
-                </div>
-                {showHint === index && (
-                  <div className="scenario__factor-hint__text">
-                    <p className="scenario__factor-hint__text-pos">{row[1]}</p>
-                    <p className="scenario__factor-hint__text-neg">{row[2]}</p>
-                  </div>
-                )}
-              </div>
-            </FormGroup>
-          ))}
+          <div className="scenario__actionButtons">
+            <Button
+              variant="contained"
+              color="success"
+              onClick={handleBullishClick}
+              disabled={buttonsDisabled}
+            >
+              Bullish
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              disabled={buttonsDisabled}
+            >
+              Bearish
+            </Button>
+          </div>
         </div>
-      </div>
-
-      {/* LineChart for Stock Data */}
-      <div className="scenario__chart">
-        <Typography variant="h6" component="h6">
-          Stock Price Trend ({scenario.stock.symbol})
-        </Typography>
-        <LineChart
-          xAxis={[{ scaleType: "point", data: xAxisData }]}
-          series={[
-            {
-              data: seriesData,
-              label: "Closing Price",
-            },
-          ]}
-          width={600}
-          height={300}
-        />
-      </div>
-
-      <div className="scenario__actionButtons">
-        <Button variant="contained" color="success">
-          Bullish
-        </Button>
-        <Button variant="contained" color="error">
-          Bearish
-        </Button>
-      </div>
-
-      <div className="scenario__navigation">
-        <Button
-          variant="contained"
-          onClick={handlePrevious}
-          disabled={currentScenarioIndex === 0}
-        >
-          Previous
-        </Button>
-        <Button
-          variant="contained"
-          onClick={handleNext}
-          disabled={currentScenarioIndex === financialSummaryData.length - 1}
-        >
-          Next
-        </Button>
-      </div>
-    </div>
+      </section>
+      {showResponse && (
+        <section className="response">
+          {/* LineChart for Stock Data */}
+          <div className="scenario__chart">
+            <Typography variant="h6" component="h6">
+              Stock Price Trend ({scenario.stock.symbol})
+            </Typography>
+            <LineChart
+              xAxis={[{ scaleType: "point", data: xAxisData }]}
+              series={[
+                {
+                  data: seriesData,
+                  label: "Closing Price",
+                },
+              ]}
+              width={600}
+              height={300}
+            />
+          </div>
+          <div className="response__buttons">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleReinvest}
+            >
+              Re-invest
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleHomeClick}
+            >
+              Home
+            </Button>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
 export default Scenario;
+
+// const handleNext = () => {
+//   if (currentScenarioIndex < financialSummaryData.length - 1) {
+//     setCurrentScenarioIndex(currentScenarioIndex + 1);
+//   }
+// };
+
+// const handlePrevious = () => {
+//   if (currentScenarioIndex > 0) {
+//     setCurrentScenarioIndex(currentScenarioIndex - 1);
+//   }
+// };
+
+{
+  /* <div className="scenario__navigation">
+            <Button
+              variant="contained"
+              onClick={handlePrevious}
+              disabled={currentScenarioIndex === 0}
+            >
+              Previous
+            </Button>
+            <Button
+              variant="contained"
+              onClick={handleNext}
+              disabled={
+                currentScenarioIndex === financialSummaryData.length - 1
+              }
+            >
+              Next
+            </Button>
+          </div> */
+}
